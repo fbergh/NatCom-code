178f063e70a2164ad1e3526b08517a0f540b5e6d
odysseas <OdysseasKr@users.noreply.github.com>
2020-03-12 05:19:22 +0100

    Add prediction collection

--- assignment3/negative-selection/prep-freek.py
+++ assignment3/negative-selection/prep-freek.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import numpy as np
 
 FILE_PATH = "syscalls"
 SYSCALLS = ["snd-cert", "snd-unm"]
@@ -35,7 +36,7 @@ def split_train_into_chunks(chunk_size):
         with open(snd_train_path) as train_file:
             for syscall in train_file:
                 # Generate all n-grams of the current syscall
-                n_grams = extract_unique_n_grams(syscall,chunk_size)
+                n_grams = extract_n_grams(syscall,chunk_size,unique=True)
                 # Write n-grams to syscall chunks file
                 syscalls_split_file.writelines(n_grams)
         syscalls_split_file.close()
@@ -58,7 +59,7 @@ def split_test_into_chunks(chunk_size):
             with open(filename+".test") as syscalls_file:
                 for syscall in syscalls_file:
                     # Generate all n-grams of the current syscall
-                    n_grams = extract_unique_n_grams(syscall,chunk_size)
+                    n_grams = extract_n_grams(syscall,chunk_size,unique=False)
                     # Write n-grams to syscall chunks file
                     syscalls_split_file.writelines(n_grams)
                     # Keep track of end position in chunks file of current syscall
@@ -69,16 +70,20 @@ def split_test_into_chunks(chunk_size):
     return syscall_type_dict
 
 
-def extract_unique_n_grams(string, n):
+def extract_n_grams(string, n, unique=True):
     """
-    Extracts unique n-grams from the current string
-    NOTE: it only extracts unique n-grams in the current string, there might still be duplicates in the collection
+    Extracts n-grams from the current string
+    NOTE: when unique=True, it only extracts unique n-grams in the current string.
+          There might still be duplicates in the collection
     """
     n_grams = []
     for i in range(len(string)-n+1):
-        n_gram = string[i:i+n]
-        if n_gram not in n_grams:
-            n_grams.append(n_gram+"\n")
+        n_gram = string[i:i+n] + "\n"
+        n_grams.append(n_gram)
+
+    if unique:
+        n_grams = list(set(n_grams))
+
     return n_grams
 
 
@@ -98,24 +103,50 @@ def merge_test_files():
         merged_file.close()
 
 
-def negative_selection(n, r, count=True, log=False):
-    """
-    TODO
-    """
-    # alphabet_file = f"file://{FILE_PATH}/{syscall_type}/{syscall_type}.alpha"
-    # self_file = f"{syscall_type}.train"
-    # os.system(f"java -jar negsel2.jar -alphabet {alphabet_file} -self {self_file} -n {n} -r {r} < merged > output")
+def negative_selection(syscall_type, n, r, count=True, log=False):
+    alphabet_file = f"file://{FILE_PATH}/{syscall_type}/{syscall_type}.alpha"
+    self_file = f"{syscall_type}-split.train"
+    os.system(f"java -jar negsel2.jar -alphabet {alphabet_file} -self {self_file} -n {n} -r {r} < {syscall_type}-merged-split.test > {syscall_type}-output")
 
+
+def collect_predictions(syscall_type, syscall_type_dict):
+    output_file = open(f"{syscall_type}-output", "r")
+    indexes = syscall_type_dict[f"{syscall_type}-self"]
+    nonself_indexes = syscall_type_dict[f"{syscall_type}-nonself"]
+    nonself_indexes = np.array(nonself_indexes) + indexes[-1]
+    indexes.extend(nonself_indexes.tolist())
+
+    pred = []
+    start = 0
+    for i in indexes:
+        # Read n lines from the output file
+        results = [float(next(output_file).strip()) for x in range(i-start)]
+        total_anomaly = np.mean(results)
+        pred.append(total_anomaly)
+        start = i
+
+    return pred
 
 def main():
     CHUNK_SIZE = 7
+
+    print("Preparing files")
     create_self_nonself_files()
     syscall_type_dict = split_test_into_chunks(CHUNK_SIZE)
     split_train_into_chunks(CHUNK_SIZE)
     merge_test_files()
 
-    negative_selection(CHUNK_SIZE, 4, count=True, log=False)
+    print("Negative selection")
+    for syscall_type in SYSCALLS:
+        negative_selection(syscall_type, CHUNK_SIZE, 4, count=True, log=False)
 
+    print("Collect predictions")
+    pred = collect_predictions(SYSCALLS[0], syscall_type_dict)
+    print(len(pred))
+
+    print("Calculate results")
+
+    print("Done")
 
 if __name__ == "__main__":
     main()
