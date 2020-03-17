80e27916c2f32f5acbde9e0b7a7f55b1df87fdb9
fbergh <freekbergh@gmail.com>
2020-03-17 18:04:42 +0100

    Created experiment function, bugfixes, and created temp/ directory for unnecessary datafiles

--- assignment3/negative-selection/prep.py
+++ assignment3/negative-selection/prep.py
@@ -1,9 +1,13 @@
-import os
+import os, shutil
+import pickle
 import subprocess
 import numpy as np
 from sklearn.metrics import roc_curve, roc_auc_score
 import matplotlib.pyplot as plt
 
+TEMP_DIR = "temp"
+OUT_DIR = "out"
+IMG_DIR = f"{OUT_DIR}/img"
 FILE_PATH = "syscalls"
 SYSCALLS = ["snd-cert", "snd-unm"]
 LABELS = ["self","nonself"]
@@ -13,8 +17,8 @@ def create_self_nonself_files():
     Splits the original snd-cert/unm.x.test files into self and non-self files
     """
     for syscall_type in SYSCALLS:
-        self_file = open(f"{syscall_type}-self.test", "w")
-        nonself_file = open(f"{syscall_type}-nonself.test", "w")
+        self_file = open(f"{TEMP_DIR}/{syscall_type}-self.test", "w")
+        nonself_file = open(f"{TEMP_DIR}/{syscall_type}-nonself.test", "w")
         snd_path = f"{FILE_PATH}/{syscall_type}/{syscall_type}"
         for file_nr in range(1,4):
             with open(f"{snd_path}.{file_nr}.test") as test_file, open(f"{snd_path}.{file_nr}.labels") as labels_file:
@@ -33,7 +37,7 @@ def split_train_into_chunks(chunk_size):
     Splits the train files into chunks
     """
     for syscall_type in SYSCALLS:
-        syscalls_split_file = open(f"{syscall_type}-split.train", "w")
+        syscalls_split_file = open(f"{TEMP_DIR}/{syscall_type}-split.train", "w")
         snd_train_path = f"{FILE_PATH}/{syscall_type}/{syscall_type}.train"
         with open(snd_train_path) as train_file:
             for syscall in train_file:
@@ -57,7 +61,7 @@ def split_test_into_chunks(chunk_size):
         for label in LABELS:
             syscall_positions = []
             syscall_pos = 0
-            filename = f"{syscall_type}-{label}"
+            filename = f"{TEMP_DIR}/{syscall_type}-{label}"
             # Create file for the syscall chunks
             syscalls_split_file = open(filename+"-split.test", "w")
             with open(filename+".test") as syscalls_file:
@@ -71,7 +75,7 @@ def split_test_into_chunks(chunk_size):
                     # Keep track of end position in chunks file of current syscall
                     syscall_pos += len(n_grams)
                     syscall_positions.append(syscall_pos)
-            syscall_type_dict[filename] = syscall_positions
+            syscall_type_dict[f"{syscall_type}-{label}"] = syscall_positions
             syscalls_split_file.close()
     return syscall_type_dict
 
@@ -99,9 +103,9 @@ def merge_test_files():
     Merges the self and nonself .test files created by split_test_into_chunks
     """
     for syscall_type in SYSCALLS:
-        self_file = open(f"{syscall_type}-self-split.test")
-        nonself_file = open(f"{syscall_type}-nonself-split.test")
-        merged_file = open(f"{syscall_type}-merged-split.test", "w")
+        self_file = open(f"{TEMP_DIR}/{syscall_type}-self-split.test")
+        nonself_file = open(f"{TEMP_DIR}/{syscall_type}-nonself-split.test")
+        merged_file = open(f"{TEMP_DIR}/{syscall_type}-merged-split.test", "w")
         merged_lines = self_file.readlines()
         merged_lines.extend(nonself_file.readlines())
         merged_file.writelines(merged_lines)
@@ -112,15 +116,16 @@ def merge_test_files():
 
 def negative_selection(syscall_type, n, r, count=True, log=False):
     alphabet_file = f"file://{FILE_PATH}/{syscall_type}/{syscall_type}.alpha"
-    self_file = f"{syscall_type}-split.train"
+    self_file = f"{TEMP_DIR}/{syscall_type}-split.train"
     count_param = "-c" if count else ""
     log_param = "-l" if log else ""
     os.system(f"java -jar negsel2.jar -alphabet {alphabet_file} -self {self_file} -n {n} -r {r} \
-                {count_param} {log_param} -c < {syscall_type}-merged-split.test > {syscall_type}-output")
+                {count_param} {log_param} < {TEMP_DIR}/{syscall_type}-merged-split.test \
+                > {OUT_DIR}/N={n}-R={r}-{syscall_type}-output")
 
 
-def collect_predictions(syscall_type, syscall_type_dict):
-    output_file = open(f"{syscall_type}-output", "r")
+def collect_predictions(n, r, syscall_type, syscall_type_dict):
+    output_file = open(f"{OUT_DIR}//N={n}-R={r}-{syscall_type}-output", "r")
     indexes = syscall_type_dict[f"{syscall_type}-self"]
     nonself_indexes = syscall_type_dict[f"{syscall_type}-nonself"]
     nonself_indexes = np.array(nonself_indexes) + indexes[-1]
@@ -146,35 +151,54 @@ def generate_roc(ground, pred):
     plt.ylabel('True Positive Rate')
     plt.legend()
 
-def main():
-    CHUNK_SIZE = 7
-    R = 4
+def run_negative_selection(n, r):
+    # Preparation
+    if not os.path.exists(TEMP_DIR):
+        os.makedirs(TEMP_DIR)
+    if not os.path.exists(OUT_DIR) or not os.path.exists(IMG_DIR):
+        os.makedirs(IMG_DIR)
 
     print("Preparing files")
     create_self_nonself_files()
-    syscall_type_dict = split_test_into_chunks(CHUNK_SIZE)
-    split_train_into_chunks(CHUNK_SIZE)
+    syscall_type_dict = split_test_into_chunks(n)
+    # Write syscall_type_dict for reproducibility
+    with open(f"{OUT_DIR}/N={n}-R={r}-indices.pkl", 'wb') as f:
+        pickle.dump(OUT_DIR, f, pickle.HIGHEST_PROTOCOL)
+    split_train_into_chunks(n)
     merge_test_files()
 
     for syscall_type in SYSCALLS:
-        print(f"Negative selection for {syscall_type}")
-        negative_selection(syscall_type, CHUNK_SIZE, R, count=False, log=False)
+        print(f"Negative selection for {syscall_type} (may take several minutes)")
+        negative_selection(syscall_type, n, r, count=True, log=True)
 
     print("Calculate results")
     for syscall_type in SYSCALLS:
-        pred = collect_predictions(syscall_type, syscall_type_dict)
+        pred = collect_predictions(n, r, syscall_type, syscall_type_dict)
         plt.figure(figsize=(8,8))
         plt.hist(pred)
-        plt.savefig("{syscall_type}-hist.pdf")
+        plt.savefig(f"{IMG_DIR}/N={n}-R={r}-{syscall_type}-hist.pdf")
+        plt.savefig(f"{IMG_DIR}/N={n}-R={r}-{syscall_type}-hist.png")
         # Ground truth predictions
         ground = np.zeros(len(pred))
         num_of_nonself = len(syscall_type_dict[f"{syscall_type}-nonself"])
         ground[-num_of_nonself:] = 1
         # Generate ROC plots
         generate_roc(ground, pred)
-        plt.savefig(f"{syscall_type}-roc.pdf")
+        plt.savefig(f"{IMG_DIR}/N={n}-R={r}-{syscall_type}-roc.pdf")
+        plt.savefig(f"{IMG_DIR}/N={n}-R={r}-{syscall_type}-roc.png")
+    
+    # Clean-up
+    shutil.rmtree(TEMP_DIR)
 
     print("Done")
+
+def main():
+    n_list = [3,5,7]
+    r_list = [2,4,6]
+
+    for n, r in zip(n_list, r_list):
+        print(f"Running negsel with N={n} and R={r}")
+        run_negative_selection(n, r)
 
 if __name__ == "__main__":
     main()
